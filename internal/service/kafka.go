ce29b73c0523bde045f0d5d076df39a30ffa2d6b
qy <jimmy@ffglobaltech.com>
2025-08-12 17:19:20 +0800

    backup

--- internal/service/kafka.go
+++ internal/service/kafka.go
@@ -4,7 +4,7 @@ import "github.com/segmentio/kafka-go"
 
 func NewKafkaReader() *kafka.Reader {
 	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{"localhost:9092"},
-		Topic:   "test",
+		Brokers: []string{"localhost:29092"},
+		Topic:   "pg.public.tuples",
 	})
 }
