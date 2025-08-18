56991cbd4a487a005f374ed41af0a4c81bde5aea
qy <jimmy@ffglobaltech.com>
2025-08-18 18:36:28 +0800

    backup

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"authz/cmd"
-	// _ "authz/docs/openapi"
 
 	_ "ariga.io/atlas-provider-gorm/gormschema"
 )
 
-// @securityDefinitions.apikey	Bearer
-// @in							header
-// @name						Authorization.
 func main() {
 	cmd.Execute()
 }
