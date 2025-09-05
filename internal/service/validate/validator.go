9b6e55e52030f6b19bd97bf54ff3f1bbe0875396
qy <jimmy@ffglobaltech.com>
2025-09-05 18:24:50 +0800

    backup

--- internal/service/validate/validator.go
+++ internal/service/validate/validator.go
@@ -2,15 +2,14 @@ package validate
 
 import (
 	"authz/internal/util"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/skyrocket-qy/erx"
-	"github.com/skyrocket-qy/gox/logx"
 )
 
 var v *validator.Validate
 
 func New() {
-	logx.Info("InitValidator")
 
 	v = validator.New(validator.WithRequiredStructEnabled())
 }
